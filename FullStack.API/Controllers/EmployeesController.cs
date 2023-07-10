167121c2d6beac11b1601598b9a71b1edd7ea27c
Olar-Alex-Student <alex.olar@student.usv.ro>
2023-07-10 17:04:51 +0300

    Creat API basic

---
+++ FullStack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using FullStack.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDBContext fullStackDBContext;
+        public EmployeesController(FullStackDBContext fullStackDBContext)
+        {
+            this.fullStackDBContext = fullStackDBContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await fullStackDBContext.Employees.ToListAsync();
+            
+            return Ok(employees);
+        }
+    }
+}
