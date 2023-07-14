c88096179fa1e893f1d661db189071916461866e
Olar-Alex-Student <alex.olar@student.usv.ro>
2023-07-14 16:57:52 +0300

    Integrat Auth0 pt. API

--- FullStack.API/Models/Image.cs
+++ FullStack.API/Models/Image.cs
@@ -3,7 +3,6 @@
     public class Image
     {
         public Guid Id { get; set; }
-
         public string Name { get; set; }
         public string Extension { get; set; }
         public string Path { get; set; }
--- FullStack.API/Program.cs
+++ FullStack.API/Program.cs
@@ -10,6 +10,39 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add Auth0
+/*
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+    options.Audience = builder.Configuration["Auth0:Audience"];
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = ClaimTypes.NameIdentifier
+    };
+});
+
+builder.Services
+  .AddAuthorization(options =>
+  {
+      options.AddPolicy(
+        "read:messages",
+        policy => policy.Requirements.Add(
+          new HasScopeRequirement("read:messages", domain)
+        )
+      );
+  });
+
+builder.Services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+*/
+//End Add Auth0
+
 builder.Services.AddDbContext<FullStackDBContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("FullStackConnectionString")
