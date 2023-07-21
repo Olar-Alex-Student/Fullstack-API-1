c87bc9d67616f874bf336db4df2038060034faa6
Olar-Alex-Student <alex.olar@student.usv.ro>
2023-07-21 16:51:03 +0300

    Creat controller pt departament

---
+++ FullStack.API/Controllers/DepartmentsController.cs
@@ -0,0 +1,125 @@
+﻿using FullStack.API.Data;
+using FullStack.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentsController : Controller
+    {
+        private readonly FullStackDBContext fullStackDBContext;
+        public DepartmentsController(FullStackDBContext fullStackDBContext)
+        {
+            this.fullStackDBContext = fullStackDBContext;
+        }
+
+        /* Get Toate Departamentele */
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDepartments()
+        {
+            var departments = await fullStackDBContext.Departments.ToListAsync();
+
+            return Ok(departments);
+        }
+
+        /* Post Departament */
+
+        [HttpPost]
+        public async Task<IActionResult> AddDepartment([FromBody] Department departmentRequest)
+        {
+            if (departmentRequest == null)
+            {
+                return BadRequest("Employee request null");
+            }
+            //Id unic
+            departmentRequest.DepartmentId = Guid.NewGuid();
+
+            await fullStackDBContext.Departments.AddAsync(departmentRequest);
+            await fullStackDBContext.SaveChangesAsync();
+
+            return Ok(departmentRequest);
+        }
+
+        /* GET 1 Departament */
+
+        [HttpGet]
+
+        // ID-ul Departamentului
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetDepartment([FromRoute] Guid id)
+        {
+            var department = await fullStackDBContext.Departments.FirstOrDefaultAsync(x => x.DepartmentId == id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(department);
+        }
+
+        /* PUT Departament */
+
+        [HttpPut]
+
+        // ID-ul Departamentului
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateDepartment([FromRoute] Guid id, Department updateDepartmentRequest)
+        {
+            // Asignarea departamentului cu datele din baza de date
+
+            var department = await fullStackDBContext.Employees.FindAsync(id);
+
+            // Verificarea daca departamentul este null
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            // Modificarea datelor departamentului
+
+            department.Name = updateDepartmentRequest.Name;
+            department.DepartmentId = updateDepartmentRequest.DepartmentId;
+
+            // Actualizarea bazei de date
+            await fullStackDBContext.SaveChangesAsync();
+
+            // Return status 
+            return Ok(department);
+        }
+
+        /* DELETE Departament */
+
+        [HttpDelete]
+
+        // ID-ul Department
+        [Route("{id:Guid}")]
+
+        public async Task<IActionResult> DeleteDepartment([FromRoute] Guid id)
+        {
+            // Asignarea Department cu datele din baza de date
+
+            var department = await fullStackDBContext.Employees.FindAsync(id);
+
+            // Verificarea daca angajatul este null
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            // Stergerea Departamentului din baza de date
+            fullStackDBContext.Employees.Remove(department);
+
+            // Actualizarea bazei de date
+            await fullStackDBContext.SaveChangesAsync();
+
+            // Return status 
+            return Ok(department);
+        }
+    }
+}
